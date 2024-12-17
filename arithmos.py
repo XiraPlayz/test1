6a130097ba00ad7ccac710f2efacf15017295df2
XiraPlayz <103579877+XiraPlayz@users.noreply.github.com>
2024-12-17 15:20:02 +0200

    Add files via upload

---
+++ arithmos.py
@@ -0,0 +1,9 @@
+#’σκηση 3
+
+number = int(input ("Δώσε έναν ακέραιο αριθμό: "))
+
+prohgoumenos = number - 1
+epomenos = number + 1
+
+print "Ο προηγούμενός του είναι " , prohgoumenos
+print "Ο επόμενός του είναι " , epomenos
